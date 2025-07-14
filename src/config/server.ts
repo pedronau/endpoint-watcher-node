3b725a97f9d5abb79ea7d447253ffd78c363b2b4
Pedro <pedropefer34@gmail.com>
2025-07-14 17:11:23 +0200

    implementamos la comprobacion periodica con la libreria cron

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,24 +1,27 @@
 import { LogEntity, LogEntityStatus } from "./entities/log.entity";
+import { CronPlugin } from "./plugins/cron.plugin";
 import { CheckService } from "./services/check.service";
 import { FileSystem } from "./services/filesystem.service";
 
 export class ServerApp {
   public static async start(url: string) {
-    const isWorking = await new CheckService().watch(url);
-    if (isWorking) {
-      new FileSystem().saveSuccessLogs(
-        new LogEntity({
-          status: LogEntityStatus.success,
-          message: `El endpoint ${url} funciona correctamente`,
-        })
-      );
-    } else {
-      new FileSystem().saveErrorLogs(
-        new LogEntity({
-          status: LogEntityStatus.error,
-          message: `El endpoint ${url} no está funcionando`,
-        })
-      );
-    }
+    CronPlugin.createJob("*/30 * * * * *", async () => {
+      const isWorking = await new CheckService().watch(url);
+      if (isWorking) {
+        new FileSystem().saveSuccessLogs(
+          new LogEntity({
+            status: LogEntityStatus.success,
+            message: `El endpoint ${url} funciona correctamente`,
+          })
+        );
+      } else {
+        new FileSystem().saveErrorLogs(
+          new LogEntity({
+            status: LogEntityStatus.error,
+            message: `El endpoint ${url} no está funcionando`,
+          })
+        );
+      }
+    });
   }
 }
