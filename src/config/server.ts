3d50e03e28fd5d287297666f1c4d30bd72f17027
Pedro <pedropefer34@gmail.com>
2025-07-14 15:58:23 +0200

    comentario

--- src/config/server.ts
+++ src/config/server.ts
@@ -4,7 +4,9 @@ export class ServerApp {
   public static async start(url: string) {
     const isWorking = await new CheckService().watch(url);
     if (isWorking) {
-        //TODO: aqui tiene que hacer la creacion del archivo de logs
+        //TODO: aqui tiene que meter el aviso en un archivo de logs de succes
+    } else {
+        //TODO: aqui lo que sucede cuando hay un error: mandar un correo y ademas meterlo en los logs de error
     }
   }
 }
