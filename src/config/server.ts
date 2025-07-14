c6c0cea227849085f34a45fc84fe898747f3fb6b
Pedro <pedropefer34@gmail.com>
2025-07-14 16:36:43 +0200

    refactor: no se estaba poniendo la fecha sola al crear los logs

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,4 +1,4 @@
-import { LogEntityStatus } from "./entities/log.entity";
+import { LogEntity, LogEntityStatus } from "./entities/log.entity";
 import { CheckService } from "./services/check.service";
 import { FileSystem } from "./services/filesystem.service";
 
@@ -6,15 +6,19 @@ export class ServerApp {
   public static async start(url: string) {
     const isWorking = await new CheckService().watch(url);
     if (isWorking) {
-      new FileSystem().saveSuccessLogs({
-        status: LogEntityStatus.success,
-        message: `El endpoint ${url} funciona correctamente`,
-      });
+      new FileSystem().saveSuccessLogs(
+        new LogEntity({
+          status: LogEntityStatus.success,
+          message: `El endpoint ${url} funciona correctamente`,
+        })
+      );
     } else {
-      new FileSystem().saveErrorLogs({
-        status: LogEntityStatus.error,
-        message: `El endpoint ${url} no está funcionando`,
-      });
+      new FileSystem().saveErrorLogs(
+        new LogEntity({
+          status: LogEntityStatus.error,
+          message: `El endpoint ${url} no está funcionando`,
+        })
+      );
     }
   }
 }
